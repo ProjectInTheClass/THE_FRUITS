698db032707145d549b1585f8c6f251811bdb9bf
jinjukim <kimjinjuind1@hanyang.ac.kr>
2024-10-13 20:08:19 +0900

    Initial Commit

---
+++ THE_FRUITS/THE_FRUITSApp.swift
@@ -0,0 +1,17 @@
+//
+//  THE_FRUITSApp.swift
+//  THE_FRUITS
+//
+//  Created by 김진주 on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct THE_FRUITSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
