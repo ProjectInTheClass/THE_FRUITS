48553c0814d163f4eadc4f0f9adba3b0191252c8
BadaHong <badah@hanyang.ac.kr>
2024-10-28 21:47:01 +0900

    backend: connected to firebase & added gitignore

--- THE_FRUITS/THE_FRUITSApp.swift
+++ THE_FRUITS/THE_FRUITSApp.swift
@@ -5,12 +5,28 @@
 //  Created by 김진주 on 10/13/24.
 //
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 @main
 struct THE_FRUITSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    @StateObject private var firestoreManager = FireStoreManager()
+    
     var body: some Scene {
         WindowGroup {
             //ContentView()
             OnBoarding()
+                .environmentObject(firestoreManager)
             
         }
     }
---
+++ THE_FRUITS/backend/FirebaseManager.swift
@@ -0,0 +1,42 @@
+//
+//  FirebaseManager.swift
+//  THE_FRUITS
+//
+//  Created by Bada Hong on 10/28/24.
+//
+
+import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+
+class FireStoreManager: ObservableObject {
+    @Published var name: String = ""
+    @Published var username: String = ""
+    @Published var password: String = ""
+
+    init() {
+        fetchData()
+    }
+    
+    func fetchData() {
+            let db = Firestore.firestore()
+            let docRef = db.collection("seller").document("troY2ZvhHxGfrSDCIggI")
+            docRef.getDocument { (document, error) in
+                guard error == nil else {
+                    print("error", error ?? "")
+                    return
+                }
+
+                if let document = document, document.exists {
+                    let data = document.data()
+                    if let data = data {
+                        self.name = data["name"] as? String ?? ""
+                        self.username = data["username"] as? String ?? ""
+                        self.password = data["password"] as? String ?? ""
+                    }
+                }
+            }
+        }
+}
+
+
