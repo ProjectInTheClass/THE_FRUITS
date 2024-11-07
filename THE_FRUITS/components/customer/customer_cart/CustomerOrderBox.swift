26e7ca7e65f95868d949b0ff159e45bef41a00ee
jieunpark <jieun20626@gmail.com>
2024-11-07 17:41:42 +0900

    mypage service section

---
+++ THE_FRUITS/components/customer/customer_cart/CustomerOrderBox.swift
@@ -0,0 +1,18 @@
+//
+//  CustomerOrderBox.swift
+//  THE_FRUITS
+//
+//  Created by 박지은 on 11/7/24.
+//
+
+import SwiftUI
+
+struct CustomerOrderBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomerOrderBox()
+}
